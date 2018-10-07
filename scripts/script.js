eb45c25f5fc95d1cc201d724ab3199478bf54b34
GlasgowSmile04 <zacglasgow@gmail.com>
2018-10-06 20:44:47 -0700

    4 answers are now generated into an array, winning answer is picked.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,26 @@
-
 let targetColor = document.getElementById('top');
+let startBtn = document.getElementById('startBtn');
+let answers = document.getElementsByTagName('h2');
+let answerArray = generateRandomColors();
 
+startBtn.addEventListener('click', generateRandomColors);
+
+function winningColor(colorArray){
+  let rand = Math.floor(Math.random() * answers.length);
+  console.log(colorArray[rand])
+  return colorArray[rand];
+}
+
+function generateRandomColors(){
+    let colorArray = [];
+    for (let i = 0; i < answers.length; i++) {
+      colorArray.push(newColor().toUpperCase());
+      // answers[i].textContent = newColor().toUpperCase();
+    }
+    console.log(colorArray);
+    winningColor(colorArray);
+    return colorArray;
+}
 
 function newColor(){
   const randomNum = (Math.random()*16777215).toString(16).slice(0, 6);
@@ -8,5 +28,3 @@ function newColor(){
   return hexColor;
 }
 
-document.getElementById('startBtn').addEventListener('click', () => {
-  targetColor.style.backgroundColor = newColor()});
